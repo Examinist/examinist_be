5a6cb76b700fb0ecfb9736b837a2b745c6c62ad6
Mariam.youssri <mariam.youssri@trianglz.com>
2023-05-02 00:25:36 +0300

    EX-53: modifications

--- app/serializers/staff_portal/exam_template_serializer.rb
+++ app/serializers/staff_portal/exam_template_serializer.rb
@@ -2,6 +2,10 @@ class StaffPortal::ExamTemplateSerializer < ApplicationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :easy, :medium, :hard
 
+  attribute :must_update_template do |object|
+    object.easy.nil? && object.medium.nil? && object.hard.nil?
+  end
+
   attribute :question_types do |object|
     StaffPortal::QuestionTypeSerializer.new(object.question_types).to_j
   end
