3de9752a780040d9648689db0f1131aeba169fa2
Ahmed Gamal <ahmed.gamal5551.ag@gmail.com>
2023-05-08 01:20:00 +0300

    pr comment

--- app/controllers/concerns/response_handler.rb
+++ app/controllers/concerns/response_handler.rb
@@ -2,9 +2,8 @@ module ResponseHandler
   extend ActiveSupport::Concern
 
   def render_response(data, status_code, pagination: nil)
-    pages_count = pagination.present? ? pagination : nil
     render(
-      json: { status: 'success', number_of_pages: pages_count }.compact
+      json: { status: 'success', number_of_pages: pagination }.compact
               .merge(data, message: nil),
       status: status_code
     )
